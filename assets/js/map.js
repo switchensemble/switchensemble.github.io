1242ced5b6b3f3f8b3800962e66598efa44565a6
Chris <christopher.e.chandler@gmail.com>
2017-10-15 22:23:47 -0400

    google maps working for concert pages!

---
+++ assets/js/map.js
@@ -0,0 +1,41 @@
+var geocoder;
+var map;
+var mapOptions = {
+    zoom: 15,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+var marker;
+
+function initialize() {
+  geocoder = new google.maps.Geocoder();
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  codeAddress();
+}
+
+function codeAddress() {
+  var address = document.getElementById('address').value;
+  geocoder.geocode( { 'address': address}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      map.setCenter(results[0].geometry.location);
+      if(marker)
+        marker.setMap(null);
+      marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location,
+          draggable: true
+      });
+      google.maps.event.addListener(marker, "dragend", function() {
+        document.getElementById('lat').value = marker.getPosition().lat();
+        document.getElementById('lng').value = marker.getPosition().lng();
+      });
+      document.getElementById('lat').value = marker.getPosition().lat();
+      document.getElementById('lng').value = marker.getPosition().lng();
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
+
+$(document).ready(function(){
+  initialize();
+});
